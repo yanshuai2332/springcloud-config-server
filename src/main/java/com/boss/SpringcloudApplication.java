6d2e267c2a3e2b7982021f7ac15532d651303a57
yanshuai <yanshuai@shangweiec.com>
2017-12-28 10:26:34 +0800

    no message

---
+++ src/main/java/com/boss/SpringcloudApplication.java
@@ -0,0 +1,14 @@
+package com.boss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudApplication.class, args);
+	}
+}
